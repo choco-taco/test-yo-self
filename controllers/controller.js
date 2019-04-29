401e7278bcbdea8d0aa00dbae00fa5a9c63ed009
Joseph Serrato <joseph.serrato@gmail.com>
2019-04-29 14:27:23 -0700

    started handlebars

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,4 +6,12 @@ router.get("/", function (req, res) {
     res.render("index");
 });
 
+router.get("/login", function (req, res) {
+    res.render("login");
+});
+
+router.get("/user", function (req, res) {
+    res.render("user");
+});
+
 module.exports = router;
