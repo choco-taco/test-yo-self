1d1980cf8ccd7b533a3fec1667887249c9a4c84c
Yuke <ray.b.hinchliffe@gmail.com>
2019-05-01 02:06:44 -0700

    realtime study problems

--- server/app.js
+++ server/app.js
@@ -12,14 +12,18 @@ app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.engine("handlebars", handlebars({ defaultLayout: "main" }))
+app.engine("handlebars", handlebars({ 
+    defaultLayout: "main",
+    helpers: {
+        'check': function(arg1, arg2, options) {
+            return (arg1 !== arg2) ? options.fn(this) : options.inverse(this)
+        }
+    }
+}))
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "handlebars")
 
 app.get('/', (req, res) => {
-
-    console.log(req.user)
-
     res.render('index')
 })
 
